0f96e4935730f9ba5383b8f565a4b04cfdb00d4e
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2021-10-03 20:34:35 +0800

    updated ui for result and detailed result views

--- lib/ui/views/user/home/prediction/result/detailedresult_view.dart
+++ lib/ui/views/user/home/prediction/result/detailedresult_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class DetailedResultView extends StatelessWidget {
   static const dummyString =
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras metus elit, egestas vel aliquet nec, rhoncus in dui. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam elementum urna vel nisl bibendum suscipit";
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras metus elit, egestas vel aliquet nec, rhoncus in dui. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam elementum urna vel nisl bibendum suscipit\n\n";
   static const longString =
-      """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus luctus magna vitae porta aliquam. Integer rhoncus sit amet tortor quis fringilla. Curabitur sed nibh sed nisi malesuada finibus eget in nisl. Suspendisse potenti. Aliquam nec nulla quis odio fringilla scelerisque. Nulla auctor felis vel rhoncus tempor. Nulla finibus, ligula id maximus posuere, quam nisl aliquet libero, nec dignissim orci nisl vel nibh.
+      """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus luctus magna vitae porta aliquam. Integer rhoncus sit amet tortor quis fringilla. Curabitur sed nibh sed nisi malesuada finibus eget in nisl. Suspendisse potenti. Aliquam nec nulla quis odio fringilla scelerisque. Nulla auctor felis vel rhoncus tempor. Nulla finibus, ligula id maximus posuere, quam nisl aliquet libero, nec dignissim orci nisl vel nibh.\n\n
 Sed magna ligula, pellentesque sodales pharetra ac, tristique in felis. Pellentesque placerat ultrices enim, vel vulputate enim maximus ut. Vivamus sit amet tincidunt turpis. Quisque iaculis enim magna, ac ornare sem consequat quis. Curabitur pulvinar fringilla consequat. Ut sit amet suscipit turpis. In in varius dui. Pellentesque malesuada sem et justo cursus, at varius justo aliquet.""";
 
   const DetailedResultView({Key key}) : super(key: key);
@@ -14,30 +15,34 @@ Sed magna ligula, pellentesque sodales pharetra ac, tristique in felis. Pellente
     return Scaffold(
       appBar: AppBar(
         elevation: 1,
-        title: Text("Prediction Result"),
+        title: Text("Detailed Information"),
       ),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Card(
+            color: Colors.blueGrey[50],
             margin: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                ListTile(
-                  leading: Icon(Icons.info),
-                  title: Text("Disease Name"),
-                  subtitle: Text("Disease Probability"),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Text(dummyString),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
-                  child: Text(longString),
-                ),
-              ],
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  ListTile(
+                    leading: Icon(MdiIcons.diabetes),
+                    title: Text("Disease Name"),
+                    subtitle: Text("Disease Probability"),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text(dummyString),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+                    child: Text(longString),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/ui/widgets/disease_card.dart
+++ lib/ui/widgets/disease_card.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:knowyourplate/ui/views/user/home/prediction/result/detailedresult_view.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class DiseaseCard extends StatelessWidget {
-  const DiseaseCard({Key key}) : super(key: key);
+  const DiseaseCard({
+    Key key,
+    @required this.background,
+    @required this.Disease,
+  }) : super(key: key);
+
+  final Color background;
+  final String Disease;
+
   // TODO: Replace with actual data
   static const dummy_string =
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras metus elit, egestas vel aliquet nec, rhoncus in dui. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam elementum urna vel nisl bibendum suscipit";
@@ -9,14 +19,14 @@ class DiseaseCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.green,
+      color: Colors.blueGrey[50],
       margin: const EdgeInsets.all(16.0),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const ListTile(
-            leading: Icon(Icons.info),
+            leading: Icon(MdiIcons.diabetes),
             title: Text("Disease Name"),
             subtitle: Text("Disease Probability"),
           ),
@@ -30,8 +40,18 @@ class DiseaseCard extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      textStyle: const TextStyle(fontSize: 10),
+                      primary: Colors.green[700],
+                      padding: EdgeInsets.all(10)),
                   onPressed: () {
                     print("Okay");
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => DetailedResultView()),
+                    );
                   },
                   child: const Text("More Details"),
                 ),
