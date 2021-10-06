fcd3143c5b7757c89a981a09bff9f43450a94729
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2021-10-07 05:07:29 +0800

    fixed overflow in record content

--- lib/ui/widgets/smart_widgets/user/record_content.dart
+++ lib/ui/widgets/smart_widgets/user/record_content.dart
@@ -19,42 +19,42 @@ class RecordContent extends StatelessWidget {
       appBar: AppBar(
         title: Text("Record Details"),
       ),
-      resizeToAvoidBottomInset: false,
       body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(height: 30),
-            Text("Food Title: ${record.title}"),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Image(
-                image: CachedNetworkImageProvider(record.pictureUrl),
-                width: 200,
-                height: 200,
-                fit: BoxFit.fitHeight,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 30),
+              Text("Food Title: ${record.title}"),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Image(
+                  image: CachedNetworkImageProvider(record.pictureUrl),
+                  width: 200,
+                  height: 200,
+                  fit: BoxFit.fitHeight,
+                ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Carbs: ${record.carbs}"),
-                SizedBox(width: 10),
-                Text("Protein: ${record.protein}"),
-                SizedBox(width: 10),
-                Text("Fats: ${record.fats}"),
-              ],
-            ),
-            SizedBox(height: 30),
-            Text("Ingredients:"),
-            SizedBox(height: 10),
-            for (var ingredient in record.ingredients)
-              Text(
-                  '${ingredient.name[0].toUpperCase()}${ingredient.name.substring(1)} : ${ingredient.quantity}'),
-            Spacer(flex: 1),
-            Text("Record of ${record.userEmail}"),
-            Text("Added on ${_formatter.formatDate(record.date)}"),
-            SizedBox(height: 100),
-          ],
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Carbs: ${record.carbs}"),
+                  SizedBox(width: 10),
+                  Text("Protein: ${record.protein}"),
+                  SizedBox(width: 10),
+                  Text("Fats: ${record.fats}"),
+                ],
+              ),
+              SizedBox(height: 30),
+              Text("Ingredients:"),
+              SizedBox(height: 10),
+              for (var ingredient in record.ingredients)
+                Text(
+                    '${ingredient.name[0].toUpperCase()}${ingredient.name.substring(1)} : ${ingredient.quantity}'),
+              SizedBox(height: 50),
+              Text("Record of ${record.userEmail}"),
+              Text("Added on ${_formatter.formatDate(record.date)}"),
+            ],
+          ),
         ),
       ),
     );
