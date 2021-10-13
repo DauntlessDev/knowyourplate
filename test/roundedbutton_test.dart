2118b0c78af9fee4efd36b655d1cb05b942ca615
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2021-10-14 02:58:21 +0800

    added ui test for rounded button

---
+++ test/roundedbutton_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:knowyourplate/ui/widgets/roundedbutton.dart';
+
+void main() {
+  testWidgets('On pressed callback', (WidgetTester tester) async {
+    var pressed = false;
+    await tester.pumpWidget(
+      MaterialApp(
+        home: RoundedButton(
+          text: 'tap me',
+          onPressed: () => pressed = true,
+          color: Colors.blue,
+        ),
+      ),
+    );
+    final button = find.byType(RaisedButton);
+    expect(button, findsOneWidget);
+    expect(find.text('tap me'), findsOneWidget);
+
+    await tester.tap(button);
+    expect(pressed, true);
+  });
+}
