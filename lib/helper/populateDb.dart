ed27d9f5a500a3e0f185fca1e1bc632950e81a44
lonemartin <john_els_05@yahoo.com>
2021-10-27 20:21:23 +0800

    yuwi records

--- lib/helper/populateDb.dart
+++ lib/helper/populateDb.dart
@@ -22,10 +22,453 @@ class PopulateDb extends BaseViewModel {
         Ingredient(name: "vegetables", quantity: 2),
         Ingredient(name: "rice", quantity: 1),
       ],
-    )
+    ),
 
     //TODO: add more records here
-    
+
+
+
+    //10-22
+    Record(
+      recordId: "rom@gmail.com2021-10-22T12:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "sinigang",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-23T16%3A08%3A19.338680.jpg?alt=media&token=22b3c6ef-0fac-4dfb-9468-928110f8d00f",
+      date: "2021-10-22T12:08:19.338680",
+      carbs: 26,
+      protein: 11,
+      fats: 1,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 1),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 1),
+      ],
+    ),
+
+    Record(
+      recordId: "rom@gmail.com2021-10-22T20:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "pinakbet",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-18T13%3A29%3A11.717793.jpg?alt=media&token=baf91229-9328-4c3f-b42f-7813513e422d",
+      date: "2021-10-22T20:08:19.338680",
+      carbs: 52,
+      protein: 22,
+      fats: 2,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 2),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 2),
+      ],
+    ),
+
+    //10-08
+
+    Record(
+      recordId: "rom@gmail.com2021-10-08T12:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "bulalo",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-23T16%3A08%3A19.338680.jpg?alt=media&token=22b3c6ef-0fac-4dfb-9468-928110f8d00f",
+      date: "2021-10-08T12:08:19.338680",
+      carbs: 26,
+      protein: 11,
+      fats: 1,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 1),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 1),
+      ],
+    ),
+
+    Record(
+      recordId: "rom@gmail.com2021-10-08T20:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "sinigang",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-23T16%3A08%3A19.338680.jpg?alt=media&token=22b3c6ef-0fac-4dfb-9468-928110f8d00f",
+      date: "2021-10-08T20:08:19.338680",
+      carbs: 26,
+      protein: 11,
+      fats: 1,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 1),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 1),
+      ],
+    ),
+
+    //10-09
+
+    Record(
+      recordId: "rom@gmail.com2021-10-09T12:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "sinigang",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-23T16%3A08%3A19.338680.jpg?alt=media&token=22b3c6ef-0fac-4dfb-9468-928110f8d00f",
+      date: "2021-10-09T12:08:19.338680",
+      carbs: 6,
+      protein: 42,
+      fats: 37.5,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 5),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 0),
+      ],
+    ),
+
+    Record(
+      recordId: "rom@gmail.com2021-10-09T20:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "sinigang",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-23T16%3A08%3A19.338680.jpg?alt=media&token=22b3c6ef-0fac-4dfb-9468-928110f8d00f",
+      date: "2021-10-09T20:08:19.338680",
+      carbs: 26,
+      protein: 11,
+      fats: 1,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 1),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 1),
+      ],
+    ),
+
+    //10-10
+    Record(
+      recordId: "rom@gmail.com2021-10-10T12:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "pinakbet",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-18T13%3A29%3A11.717793.jpg?alt=media&token=baf91229-9328-4c3f-b42f-7813513e422d",
+      date: "2021-10-10T12:08:19.338680",
+      carbs: 52,
+      protein: 22,
+      fats: 2,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 2),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 2),
+      ],
+    ),
+
+    Record(
+      recordId: "rom@gmail.com2021-10-10T20:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "bulalo",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-23T16%3A08%3A19.338680.jpg?alt=media&token=22b3c6ef-0fac-4dfb-9468-928110f8d00f",
+      date: "2021-10-10T20:08:19.338680",
+      carbs: 26,
+      protein: 11,
+      fats: 1,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 1),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 1),
+      ],
+    ),
+
+    //10-11
+    Record(
+      recordId: "rom@gmail.com2021-10-11T12:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "sinigang",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-23T16%3A08%3A19.338680.jpg?alt=media&token=22b3c6ef-0fac-4dfb-9468-928110f8d00f",
+      date: "2021-10-11T12:08:19.338680",
+      carbs: 26,
+      protein: 11,
+      fats: 1,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 1),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 1),
+      ],
+    ),
+
+    Record(
+      recordId: "rom@gmail.com2021-10-11T20:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "sinigang",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-23T16%3A08%3A19.338680.jpg?alt=media&token=22b3c6ef-0fac-4dfb-9468-928110f8d00f",
+      date: "2021-10-11T20:08:19.338680",
+      carbs: 6,
+      protein: 42,
+      fats: 37.5,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 5),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 0),
+      ],
+    ),
+
+    //10-12
+    Record(
+      recordId: "rom@gmail.com2021-10-12T12:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "bulalo",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-23T16%3A08%3A19.338680.jpg?alt=media&token=22b3c6ef-0fac-4dfb-9468-928110f8d00f",
+      date: "2021-10-12T12:08:19.338680",
+      carbs: 26,
+      protein: 11,
+      fats: 1,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 1),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 1),
+      ],
+    ),
+
+    Record(
+      recordId: "rom@gmail.com2021-10-12T20:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "pinakbet",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-18T13%3A29%3A11.717793.jpg?alt=media&token=baf91229-9328-4c3f-b42f-7813513e422d",
+      date: "2021-10-12T20:08:19.338680",
+      carbs: 52,
+      protein: 22,
+      fats: 2,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 2),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 2),
+      ],
+    ),
+
+    //10-13
+    Record(
+      recordId: "rom@gmail.com2021-10-13T12:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "sinigang",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-23T16%3A08%3A19.338680.jpg?alt=media&token=22b3c6ef-0fac-4dfb-9468-928110f8d00f",
+      date: "2021-10-13T12:08:19.338680",
+      carbs: 26,
+      protein: 11,
+      fats: 1,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 1),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 1),
+      ],
+    ),
+
+    //10-15
+    Record(
+      recordId: "rom@gmail.com2021-10-15T12:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "pinakbet",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-18T13%3A29%3A11.717793.jpg?alt=media&token=baf91229-9328-4c3f-b42f-7813513e422d",
+      date: "2021-10-15T12:08:19.338680",
+      carbs: 52,
+      protein: 22,
+      fats: 2,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 2),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 2),
+      ],
+    ),
+
+    Record(
+      recordId: "rom@gmail.com2021-10-15T20:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "bulalo",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-23T16%3A08%3A19.338680.jpg?alt=media&token=22b3c6ef-0fac-4dfb-9468-928110f8d00f",
+      date: "2021-10-15T20:08:19.338680",
+      carbs: 26,
+      protein: 11,
+      fats: 1,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 1),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 1),
+      ],
+    ),
+
+    //10-18
+
+    Record(
+      recordId: "rom@gmail.com2021-10-18T20:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "pinakbet",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-18T13%3A29%3A11.717793.jpg?alt=media&token=baf91229-9328-4c3f-b42f-7813513e422d",
+      date: "2021-10-18T20:08:19.338680",
+      carbs: 52,
+      protein: 22,
+      fats: 2,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 2),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 2),
+      ],
+    ),
+
+    //10-19
+    Record(
+      recordId: "rom@gmail.com2021-10-19T12:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "sinigang",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-23T16%3A08%3A19.338680.jpg?alt=media&token=22b3c6ef-0fac-4dfb-9468-928110f8d00f",
+      date: "2021-10-19T12:08:19.338680",
+      carbs: 26,
+      protein: 11,
+      fats: 1,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 1),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 1),
+      ],
+    ),
+
+    Record(
+      recordId: "rom@gmail.com2021-10-19T20:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "bulalo",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-23T16%3A08%3A19.338680.jpg?alt=media&token=22b3c6ef-0fac-4dfb-9468-928110f8d00f",
+      date: "2021-10-19T20:08:19.338680",
+      carbs: 26,
+      protein: 11,
+      fats: 1,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 1),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 1),
+      ],
+    ),
+
+    //10-20
+    Record(
+      recordId: "rom@gmail.com2021-10-20T20:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "sinigang",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-23T16%3A08%3A19.338680.jpg?alt=media&token=22b3c6ef-0fac-4dfb-9468-928110f8d00f",
+      date: "2021-10-20T20:08:19.338680",
+      carbs: 26,
+      protein: 11,
+      fats: 1,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 1),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 1),
+      ],
+    ),
+
+    //10-21
+    Record(
+      recordId: "rom@gmail.com2021-10-21T12:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "sinigang",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-23T16%3A08%3A19.338680.jpg?alt=media&token=22b3c6ef-0fac-4dfb-9468-928110f8d00f",
+      date: "2021-10-21T12:08:19.338680",
+      carbs: 6,
+      protein: 42,
+      fats: 37.5,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 5),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 0),
+      ],
+    ),
+
+    Record(
+      recordId: "rom@gmail.com2021-10-21T20:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "sinigang",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-23T16%3A08%3A19.338680.jpg?alt=media&token=22b3c6ef-0fac-4dfb-9468-928110f8d00f",
+      date: "2021-10-21T20:08:19.338680",
+      carbs: 26,
+      protein: 11,
+      fats: 1,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 1),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 1),
+      ],
+    ),
+
+    //10-25
+    Record(
+      recordId: "rom@gmail.com2021-10-25T12:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "pinakbet",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-18T13%3A29%3A11.717793.jpg?alt=media&token=baf91229-9328-4c3f-b42f-7813513e422d",
+      date: "2021-10-25T12:08:19.338680",
+      carbs: 52,
+      protein: 22,
+      fats: 2,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 2),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 2),
+      ],
+    ),
+
+    Record(
+      recordId: "rom@gmail.com2021-10-25T20:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "bulalo",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-23T16%3A08%3A19.338680.jpg?alt=media&token=22b3c6ef-0fac-4dfb-9468-928110f8d00f",
+      date: "2021-10-25T20:08:19.338680",
+      carbs: 26,
+      protein: 11,
+      fats: 1,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 1),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 1),
+      ],
+    ),
+
+    //10-26
+    Record(
+      recordId: "rom@gmail.com2021-10-26T12:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "sinigang",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-23T16%3A08%3A19.338680.jpg?alt=media&token=22b3c6ef-0fac-4dfb-9468-928110f8d00f",
+      date: "2021-10-26T12:08:19.338680",
+      carbs: 6,
+      protein: 42,
+      fats: 37.5,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 5),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 0),
+      ],
+    ),
+
+    Record(
+      recordId: "rom@gmail.com2021-10-26T20:08:19.338680",
+      userEmail: "rom@gmail.com",
+      title: "sinigang",
+      pictureUrl:
+      "https://firebasestorage.googleapis.com/v0/b/knowyourplate-4790c.appspot.com/o/posts%2From%40gmail.com2021-10-23T16%3A08%3A19.338680.jpg?alt=media&token=22b3c6ef-0fac-4dfb-9468-928110f8d00f",
+      date: "2021-10-26T20:08:19.338680",
+      carbs: 26,
+      protein: 11,
+      fats: 1,
+      ingredients: [
+        Ingredient(name: "meat", quantity: 1),
+        Ingredient(name: "vegetables", quantity: 2),
+        Ingredient(name: "rice", quantity: 1),
+      ],
+    ),
   ];
 
   void populateRom() {
